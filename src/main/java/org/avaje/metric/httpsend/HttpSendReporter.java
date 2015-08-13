a74a8644f2210df377e005010dfb68a9d165ac32
Robin Bygrave <robin.bygrave@gmail.com>
2015-08-14 11:49:27 +1200

    Update for MetricReporter 4.x API

--- src/main/java/org/avaje/metric/httpsend/HttpSendReporter.java
+++ src/main/java/org/avaje/metric/httpsend/HttpSendReporter.java
@@ -1,108 +1,89 @@
 package org.avaje.metric.httpsend;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
-
-import com.squareup.okhttp.*;
-
-import org.avaje.metric.Metric;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 import org.avaje.metric.report.HeaderInfo;
 import org.avaje.metric.report.JsonWriteVisitor;
 import org.avaje.metric.report.MetricReporter;
+import org.avaje.metric.report.ReportMetrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.StringWriter;
+import java.net.URL;
 
 /**
  * Http(s) based Reporter that sends JSON formatted metrics message to a Repo server.
  */
 public class HttpSendReporter implements MetricReporter {
 
-  protected final Logger log = LoggerFactory.getLogger(HttpSendReporter.class);
+  protected static final Logger logger = LoggerFactory.getLogger(HttpSendReporter.class);
 
-  private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+  protected static final String KEY_CODE = "key-code";
+
+  protected static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
   protected final OkHttpClient client = new OkHttpClient();
 
-  protected URL url;
+  protected final URL url;
 
-  protected HeaderInfo headerInfo;
-
-  public HttpSendReporter() {
-
-  }
-
-  /**
-   * Set the URL of the repository the metrics are sent to.
-   */
-  public void setUrl(URL url) {
+  public HttpSendReporter(URL url) {
     this.url = url;
-  }
-
-  /**
-   * Set the header information which identifies the specific source of the metrics.
-   */
-  public void setHeaderInfo(HeaderInfo headerInfo) {
-    this.headerInfo = headerInfo;
   }
 
   /**
    * Send the non-empty metrics that were collected to the remote repository.
    */
-  public void report(List<Metric> metrics) {
+  @Override
+  public void report(ReportMetrics reportMetrics) {
+
+    HeaderInfo headerInfo = reportMetrics.getHeaderInfo();
+    String key = headerInfo.getKey();
 
     String json = null;
-    
-    try {  
-      long collectionTime = System.currentTimeMillis();
-      json = buildJsonPayload(metrics, collectionTime);
 
-      if (log.isTraceEnabled()) {
-        log.trace("Sending:\n {}", json);
+    try {
+      StringWriter writer = new StringWriter(1000);
+      JsonWriteVisitor jsonVisitor = new JsonWriteVisitor(writer, reportMetrics);
+      jsonVisitor.write();
+
+      json = writer.toString();
+
+      if (logger.isTraceEnabled()) {
+        logger.trace("Sending:\n {}", json);
       }
-      
+
       RequestBody body = RequestBody.create(JSON, json);
       Request request = new Request.Builder()
-        .url(url)
-        .post(body)
-        .build();
+          .url(url)
+          .addHeader(KEY_CODE, key)
+          .post(body)
+          .build();
 
       Response response = client.newCall(request).execute();
       if (!response.isSuccessful()) {
-        log.info("Unsuccessful sending metrics payload to server");
+        logger.info("Unsuccessful sending metrics payload to server");
         storeJsonForResend(json);
       }
 
     } catch (Exception e) {
       // store json message in a file to resend later...
-      log.error("Exception sending metrics to server", e);
+      logger.error("Exception sending metrics to server", e);
       if (json != null) {
         storeJsonForResend(json);
       }
     }
   }
 
-  /**
-   * Trim out single and double quotes from our key header values.
-   */
-  protected String stripQuotes(String value) {
-    if (value == null) {
-      return null;
-    }
-    // quotes considered invalid for our header values
-    return value.replace("\"", "").replace("'", "").trim();
-  }
-
   protected void storeJsonForResend(String json) {
     // override this to support store and re-send 
   }
 
-  protected String buildJsonPayload(List<Metric> metrics, long collectionTime) throws IOException {
 
-    JsonWriteVisitor jsonVisitor = new JsonWriteVisitor(collectionTime);
-    return jsonVisitor.buildJson(headerInfo, metrics);
-  }
-
+  @Override
   public void cleanup() {
     // Do nothing
   }
--- src/test/java/org/avaje/metric/httpsend/HttpSendReporterTest.java
+++ src/test/java/org/avaje/metric/httpsend/HttpSendReporterTest.java
@@ -1,10 +1,62 @@
 package org.avaje.metric.httpsend;
 
-import static org.junit.Assert.*;
+import org.avaje.metric.Metric;
+import org.avaje.metric.MetricManager;
+import org.avaje.metric.TimedMetric;
+import org.avaje.metric.core.DefaultTimedMetric;
+import org.avaje.metric.report.HeaderInfo;
+import org.avaje.metric.report.ReportMetrics;
+import org.junit.Ignore;
+import org.junit.Test;
 
-/**
- * Created by rob on 14/08/15.
- */
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HttpSendReporterTest {
 
+  private static long NANOS_TO_MICROS = 1000L;
+
+  @Ignore
+  @Test
+  public void testIntegrationWithLocalRepository() throws MalformedURLException {
+
+    URL url = new URL("http://127.0.0.1:8080/");
+    HttpSendReporter reporter = new HttpSendReporter(url);
+
+    reporter.report(metrics());
+  }
+
+
+  private ReportMetrics metrics() {
+
+    HeaderInfo headerInfo = new HeaderInfo();
+    headerInfo.setApp("app1");
+    headerInfo.setEnv("PROD");
+    headerInfo.setKey("UNLOCK");
+    headerInfo.setServer("homer");
+
+    List<Metric> metrics = new ArrayList<>();
+    metrics.add(createTimedMetric());
+
+    long collectTime = System.currentTimeMillis();
+
+    return new ReportMetrics(headerInfo, collectTime, metrics);
+  }
+
+  private TimedMetric createTimedMetric() {
+
+    TimedMetric metric = new DefaultTimedMetric(MetricManager.name("org.test.TimedFoo.doStuff"));
+
+    // add duration times in nanos
+    metric.addEventDuration(true, 100 * NANOS_TO_MICROS); // 100 micros
+    metric.addEventDuration(true, 120 * NANOS_TO_MICROS); // 120 micros
+    metric.addEventDuration(true, 140 * NANOS_TO_MICROS);
+    metric.addEventDuration(false, 200 * NANOS_TO_MICROS);
+    metric.addEventDuration(false, 220 * NANOS_TO_MICROS);
+
+    metric.collectStatistics();
+    return metric;
+  }
 }
